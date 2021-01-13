cc6ed79b0bb93db81741dfcdf9c3a59e76b5c056
Hoan96 <boyf79497@gmail.com>
2021-01-13 14:56:57 +0700

    renew

--- XLANH/venv/Scripts/activate.bat
+++ XLANH/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Github_Project\XLANH\venv
+set VIRTUAL_ENV=D:\Langguese\Python\Study\OpenCV\XLANH\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
