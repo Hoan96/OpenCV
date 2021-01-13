c3b5e6661764a7d75a0ace656cb92d810e0e782d
Hoan96 <boyf79497@gmail.com>
2021-01-13 15:07:39 +0700

    update

--- XLANH/main.py
+++ XLANH/main.py
@@ -1,1 +1,17 @@
+import cv2
+import matplotlib.pyplot as plt
 
+cap = cv2.VideoCapture(0)
+
+print(cap.isOpened())
+while  (cap.isOpened()):
+    ret,frame = cap.read()
+
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('hihi',gray)
+
+    if cv2.waitKey(1) & 0xFF == ord ('x'):
+        break
+cap.release()
+cv2.destroyAllWindows()
