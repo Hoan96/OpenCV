44a36128d6d474af1ded82ef56078fc644da9e43
Hoan96 <boyf79497@gmail.com>
2021-01-13 14:48:32 +0700

    Update face.py

--- face.py
+++ face.py
@@ -3,7 +3,7 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 img = cv2.imread('watch.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 for (x,y,w,h) in faces:
 	img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 	roi_gray = gray[y:y+h, x:x+w]
